ece8b2509b3b085483bcc1740ee4febfde85baf5
chriss sarmiento <jacinto011200@gmail.com>
2024-10-30 14:12:47 +0800

    add all the data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 
     @yield('styles')
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js']);
 </head>
 
 <body class="font-sans antialiased">
