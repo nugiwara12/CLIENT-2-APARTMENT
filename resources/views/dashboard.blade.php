96f7b4505e18ddcff007e8873c035222f5bb5444
jc sarmiento <jacinto011200@gmail.com>
2024-11-01 23:01:48 +0800

    remove the login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,6 @@
     <div class="py-12">
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <!-- Logged in Message -->
-                <div id="login-message" class="mt-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                    <!-- Pending Card -->
                     <div class="bg-yellow-100 p-4 rounded-lg shadow">
