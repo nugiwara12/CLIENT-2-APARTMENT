5b9cd8674bb4d3006ecc85a514915186dcb5d770
jc sarmiento <jacinto011200@gmail.com>
2024-11-02 14:21:33 +0800

    add the icon of the the URL page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
+    <link rel="shortcut icon" href="{{ asset('admin_assets/assets/images/logo/logo.png') }}">
+    <link rel="shortcut icon" href="{{ asset('admin_assets/assets/images/logo/logo.png') }}" type="image/x-icon">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
