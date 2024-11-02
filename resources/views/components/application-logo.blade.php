e9dc7d19cacd336c02694ecbf8fd094187a6f981
jc sarmiento <jacinto011200@gmail.com>
2024-11-02 14:31:19 +0800

    add the logo of the login and register

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex justify-center items-center py-4">
     <img src="{{ asset('admin_assets/assets/images/logo/logo.png') }}" alt="Logo"
-         class="w-10 h-10 object-contain rounded-full">
+         class="w-20 h-20 object-contain rounded-full">
 </div>
