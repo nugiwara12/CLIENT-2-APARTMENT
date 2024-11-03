b68c4383c1584673bd272a8e3bc3e586e9926d94
jc sarmiento <jacinto011200@gmail.com>
2024-11-03 19:49:32 +0800

    add all the data of the button payment

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -204,7 +204,9 @@
                     @csrf
                     <div class="modal-header">
                         <h5 class="modal-title" id="createModalLabel">Add Payment</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                     <div class="modal-body overflow-auto max-h-96 overflow-y-auto">
                         <div class="mb-3">
@@ -250,7 +252,6 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </form>
